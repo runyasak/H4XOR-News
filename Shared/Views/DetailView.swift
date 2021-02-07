fb0ad98a8c938e79a9d1a35049afec370dfe037e
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-02-07 23:34:14 +0700

    feat: add DetailView with navigation

---
+++ Shared/Views/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailView.swift
+//  H4XOR News (iOS)
+//
+//  Created by Runyasak Chaengnaimuang on 7/2/2564 BE.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let url: String?
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com").previewLayout(.sizeThatFits)
+    }
+}
