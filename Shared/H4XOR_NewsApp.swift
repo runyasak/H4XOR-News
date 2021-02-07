0b5b74b5900624fd64f8a496841cd7bc1d4fe7e1
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-02-07 22:20:35 +0700

    Initial Commit

---
+++ Shared/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  Shared
+//
+//  Created by Runyasak Chaengnaimuang on 7/2/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
